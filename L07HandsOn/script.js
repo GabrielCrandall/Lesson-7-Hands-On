88367448ea56b0cf952943dd7c568d0eb565356a
GabrielCrandall <50808284+GabrielCrandall@users.noreply.github.com>
2019-05-20 12:39:31 -0700

    Add files via upload

---
+++ L07HandsOn/script.js
@@ -0,0 +1,15 @@
+﻿function regexChecker() {
+    const regex = /^[A-Z][a-zA-Z]+$/;
+    var firstName = document.getElementById('firstName').value;
+    var lastName = document.getElementById('lastName').value;
+
+    if (firstName.match(regex) && lastName.match(regex)) {
+        alert('Yay! Your inputs were all correct!');
+    }
+    else {
+        alert('Oh no! That\'s an invalid format!');
+    }
+}
+
+var btn = document.getElementById('btn');
+btn.addEventListener("click", regexChecker);
